b3f920ed6def424689c0cb16193040e4e942c8f2
Manu <Siotma@users.noreply.github.com>
2017-10-30 13:45:41 +0100

    Script actualizado

--- Hija Resurrection/guest-session-auto.sh
+++ Hija Resurrection/guest-session-auto.sh
@@ -14,42 +14,9 @@
 export TEXTDOMAINDIR=/usr/share/locale-langpack
 export TEXTDOMAIN=lightdm
 
-# disable screen locking
+disable screen locking
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 
-# info dialog about the temporary nature of a guest session
-dialog_content () {
-	TITLE=$(gettext 'Temporary Guest Session')
-	TEXT=$(gettext 'All data created during this guest session will be deleted
-when you log out, and settings will be reset to defaults.
-Please save files on some external device, for instance a
-USB stick, if you would like to access them again later.')
-	para2=$(gettext 'Another alternative is to save files in the
-/var/guest-data folder.')
-	test -w /var/guest-data && TEXT="$TEXT\n\n$para2"
-}
-test -f "$HOME"/.skip-guest-warning-dialog || {
-	if [ "$KDE_FULL_SESSION" = true ] && [ -x /usr/bin/kdialog ]; then
-		dialog_content
-		TEXT_FILE="$HOME"/.guest-session-kdialog
-		echo -n "$TEXT" > $TEXT_FILE
-		{
-			# Sleep to wait for the the info dialog to start.
-			# This way the window will likely become focused.
-			sleep $DIALOG_SLEEP
-			kdialog --title "$TITLE" --textbox $TEXT_FILE 450 250
-			rm -f $TEXT_FILE
-		} &
-	elif [ -x /usr/bin/zenity ]; then
-		dialog_contentmkdir -p ~/Desktop/aa/bb/cd
-		{
-			# Sleep to wait for the the info dialog to start.
-			# This way the window will likely become focused.
-			sleep $DIALOG_SLEEPmkdir -p ~/Desktop/aa/bb/cd
-			zenity --warning --no-wrap --title="$TITLE" --text="$TEXT"
-		} &
-	fi
-}
 
 # run possible local startup commands
 test -f /etc/guest-session/auto.sh && . /etc/guest-session/auto.sh
@@ -59,7 +26,30 @@ test -f /etc/guest-session/auto.sh && . /etc/guest-session/auto.sh
 mkdir -p ~/.mono/registry/CurrentUser/software
 cp -r /usr/share/asrob/repetier ~/.mono/registry/CurrentUser/software
 cp -r /usr/share/asrob/.Slic3r ~/
+cp -r /usr/share/asrob/Repetier-Host.desktop ~/Desktop
+cp -r /usr/share/asrob/Calibration ~/Desktop
 
-#Disable auto-logout
+#Disable auto-logout (Prueba)
 export TMOUT=0
 xset s 0 dpms 0 0 0
+
+# info dialog about the temporary nature of a guest session
+dialog_content () {
+	TITLE=$(gettext 'Temporary Guest Session')
+	TEXT=$(gettext '
+IMPORTANT NOTICE:
+All data created during this guest session will be deleted when
+you log out, and settings will be reset to defaults.
+Please save files on some external device, for instance a USB
+stick, if you would like to access them again later.
+
+
+AVISO IMPORTANTE:
+Toda la información creada en esta sesión será eliminada al
+cerrar sesión, y la configuración se restablecerá por defecto.
+Por favor, utiliza un medio extraíble, como una memoria USB,
+para salvar los archivos si no quieres perderlos.')
+}
+
+dialog_content
+zenity --info --title="$TITLE" --width=500 --height=200 --text="$TEXT" --no-wrap
