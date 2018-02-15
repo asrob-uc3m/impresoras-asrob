aa071133a66e3b28f2696151f15446f349056d01
Manuel Peña Fernández <Siotma@users.noreply.github.com>
2018-02-15 19:45:10 +0100

    PLA temperature preset adjusted

--- Hija Resurrection/Configuration.h
+++ Hija Resurrection/Configuration.h
@@ -535,8 +535,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 //#define EEPROM_CHITCHAT
 
 // Preheat Constants
-#define PLA_PREHEAT_HOTEND_TEMP 180
-#define PLA_PREHEAT_HPB_TEMP 70
+#define PLA_PREHEAT_HOTEND_TEMP 205 //HIJA_RESURRECTION
+#define PLA_PREHEAT_HPB_TEMP 50 //HIJA_RESURRECTION
 #define PLA_PREHEAT_FAN_SPEED 255   // Insert Value between 0 and 255
 
 #define ABS_PREHEAT_HOTEND_TEMP 210 //HIJA_RESURRECTION
