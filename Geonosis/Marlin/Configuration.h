ee41e94d8dfd0d02ad12fc4c19567c9574604737
Jorge Ramos <10605193+GioSunrider@users.noreply.github.com>
2019-10-31 20:37:00 +0100

    Update steps per unit

--- Geonosis/Marlin/Configuration.h
+++ Geonosis/Marlin/Configuration.h
@@ -614,7 +614,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 132 } //GEONOSIS
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 40, 40, 200, 66 } //GEONOSIS
 
 /**
  * Default Max Feed Rate (mm/s)
